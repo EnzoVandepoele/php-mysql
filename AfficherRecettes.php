7bd67c33324a53dcb033a7a33b6ad3f5ceb41b36
Enzo VANDEPOELE <ev399356@iut21.u-bourgogne.fr>
2024-09-05 11:52:00 +0200

    Les fonctions

--- AfficherRecettes.php
+++ AfficherRecettes.php
@@ -1,23 +1,47 @@
 <?php 
+   $users = [
+        [
+            'full_name' => 'Mickaël Andrieu',
+            'email' => 'mickael.andrieu@exemple.com',
+            'age' => 34,
+        ],
+        [
+            'full_name' => 'Mathieu Nebra',
+            'email' => 'mathieu.nebra@exemple.com',
+            'age' => 34,
+        ],
+        [
+            'full_name' => 'Laurène Castor',
+            'email' => 'laurene.castor@exemple.com',
+            'age' => 28,
+        ],
+    ];
+
     $recipes = [
         [
             'title' => 'Cassoulet',
-            'recipe' => 'Etape 1 : des flageolets !',
-            'author' => ['email' => 'mickael.andrieu@exemple.com', 'full_name' => 'Mickaël Andrieu', 'age' => '34'],
+            'recipe' => '',
+            'author' => 'mickael.andrieu@exemple.com',
             'is_enabled' => true,
         ],
         [
             'title' => 'Couscous',
             'recipe' => '',
-            'author' => ['email' => 'mickael.andrieu@exemple.com', 'full_name' => 'Mickaël Andrieu', 'age' => '34'],
+            'author' => 'mickael.andrieu@exemple.com',
             'is_enabled' => false,
         ],
         [
             'title' => 'Escalope milanaise',
-            'recipe' => 'Etape 1 : prenez une belle escalope',
-            'author' => ['email' => 'mathieu.nebra@exemple.com', 'full_name' => 'Mathieu Nebra', 'age' => '34'],
+            'recipe' => '',
+            'author' => 'mathieu.nebra@exemple.com',
             'is_enabled' => true,
-        ]
+        ],
+        [
+            'title' => 'Salade Romaine',
+            'recipe' => '',
+            'author' => 'laurene.castor@exemple.com',
+            'is_enabled' => false,
+        ],
     ];
 
     function isValidRecipe(array $recipe) : bool {
@@ -42,9 +66,15 @@
         return $validRecipes;
     }
 
-    function displayAuthor(array $author) : string
+    function displayAuthor(string $authorEmail, array $users) : string
     {
-        return $author['full_name'] . '(' . $author['age'] . ' ans)';
+        for ($i = 0; $i < count($users); $i++) {
+        $author = $users[$i];
+
+        if ($authorEmail === $author['email']) {
+            return $author['full_name'] . '(' . $author['age'] . ' ans)';
+        }
+    }
     }
 ?>
 
@@ -73,7 +103,7 @@
                     if (isValidRecipe($recipe)) {
                         echo "<div class='title'>" . $recipe['title'] . '</div>';
                         // echo $recipe['recipe'] . '<br>';
-                        echo '<i>' . displayAuthor($recipe['author']) . '</i>' . '<br>' . '<br>';
+                        echo '<i>' . displayAuthor($recipe['author'], $users) . '</i>' . '<br>' . '<br>';
                     }
                 }
             ?>

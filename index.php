ace548f27a2dbc1d6ed59357d82104c1ded0a3a8
EnzoVandepoele <162293370+EnzoVandepoele@users.noreply.github.com>
2024-09-25 10:58:05 +0200

    Effectuez une première requête SQL

--- index.php
+++ index.php
@@ -30,13 +30,24 @@
 
     <!-- Si l'utilisateur existe, on affiche les recettes -->
     <?php if(isset($_SESSION['LOGGED_USER'])): ?>
-        <?php foreach(getRecipes($recipes, 5) as $recipe) : ?>
+        <!-- On se connecte à MySQL -->
+        <?php include_once('mysql.php'); ?>
+        <!-- Si tout va bien, on peut continuer -->
+        <?php
+            // On récupère tout le contenu de la table recipes
+            $sqlQuery = 'SELECT * FROM recipes where is_enabled';
+            $recipesStatement = $db->prepare($sqlQuery);
+            $recipesStatement->execute();
+            $recipes = $recipesStatement->fetchAll();
+        ?>
+        <!-- On affiche chaque recette une à une -->
+        <?php foreach ($recipes as $recipe) : ?>
             <article>
-                <h3><?php echo $recipe['title']; ?></h3>
+                </br><h3><?php echo $recipe['title']; ?></h3>
                 <div><?php echo $recipe['recipe']; ?></div>
                 <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
             </article>
-        <?php endforeach ?>
+        <?php endforeach; ?>
     <?php endif; ?>
     </div>
 
